71c7907d9171f37f4452ea174678a660cc9358d5
Yinfeng Lin <yinfeng.lin@outlook.com>
2017-07-16 15:06:51 +0800

    Refactor, add waiting count, adjust request interval input

--- userscript.js
+++ userscript.js
@@ -8,103 +8,23 @@
 // @grant        none
 // ==/UserScript==
 
-let intervalIDs = [];
-
-let log = function() {};
-
-function req(form) {
-	let http = new XMLHttpRequest();
-	let url = 'xkAction.do';
-	let data = new FormData(form);
-	http.open('POST', url, true);
-
-	http.onreadystatechange = function() {
-		if(http.readyState == 4 && http.status == 200) {
-			handleResponse(http.responseText);
-		}
-	};
-	http.send(data);
-	log('Reqest sent');
-}
-
-function handleResponse(response) {
-	const pattern = /xs\('(.*)'\);/;
-	const allTagPattern = /<br[^>]*>/g;
-
-	let raw_msg = pattern.exec(response);
-	if (raw_msg && raw_msg[1]) {
-		let msg = raw_msg[1].replace(allTagPattern, '');
-		log(msg);
-		if (msg.search('成功') != -1) {
-			stopAll();
-		}
-	} else {
-		log('Unknown response');
-	}
-}
-
-function poll() {
-	let bottomFrame = document.getElementsByName('bottomFrame')[0];
-	let mainFrame = getFrameEleByName(bottomFrame, 'mainFrame');
-	let inforUpContent = getFrameEleByName(mainFrame, 'inforUpContent');
-	let form;
-	if (inforUpContent && (form = getFrameEleByName(inforUpContent, 'xkActionForm'))) {
-		return setInterval(req.bind(null, form), getInvervalInputValue());
-	} else {
-		log('Unknown error');
-	}
-}
-
-function getInvervalInputValue() {
-	let input = document.getElementById('user_script_frame').contentDocument.
-		getElementById('interval_input');
-	if (input.value < Number(input.getAttribute('value')))
-		input.value = 10;
-	return input.value;
-}
-
-function getFrameEleByName(frame, name) {
-	return frame.contentDocument.getElementsByName(name)[0];
-}
-
-function output(str) {
-	function genTimeTag() {
-		let date = new Date();
-		return '[' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ']';
-	}
-
-	let p = document.createElement('p');
-	this.insertBefore(p, this.firstChild);
-	p.innerHTML = genTimeTag() + ' ' + str;
-	return p;
-}
-
-function stopAll() {
-	intervalIDs.map(function(x){clearInterval(x);});
-	intervalIDs = [];
-}
-
-function startNew() {
-	intervalIDs.push(poll());
-}
-
-function main() {
+(function () {
 	'use strict';
 
-	let html = document.getElementsByTagName('html')[0];
-	let frameset = html.getElementsByTagName('frameset')[0];
+	var html = document.getElementsByTagName('html')[0];
+	var frameset = html.getElementsByTagName('frameset')[0];
 	if (!frameset) return;
 	frameset.setAttribute('rows', '48,*,200');
 
-	let frame = document.createElement('frame');
+	var frame = document.createElement('frame');
 	frameset.appendChild(frame);
 	frame.scrolling = 'auto';
 	frame.style.borderStyle = 'solid';
 	frame.style.borderWidth = '0.125em 0 0 0';
 	frame.setAttribute('id', 'user_script_frame');
 
-	let userScriptDoc = frame.contentDocument;
-	let body = userScriptDoc.getElementsByTagName('body')[0];
+	var userScriptDoc = frame.contentDocument;
+	var body = userScriptDoc.getElementsByTagName('body')[0];
 	body.style.font = '1em Monospace';
 	body.style.lineHeight = '20%';
 	body.style.margin = '0';
@@ -112,21 +32,143 @@ function main() {
 	body.innerHTML =`
 		<div id="button_div" style="border-bottom: 1px dashed;">
 			<button id="start_button" style="margin: 0.5em;">Start a new polling</button>
-			<input id="interval_input" type="number" value=100 min=10>
+			<button id="refresh_button" style="margin: 0.5em;">Refresh data</button>
+			<div id="interval_div" style="display: inline-block;">
+				<input id="interval_input" type="number" value=10 min=1 style="width: 5em;">
+				<span>ms</span>
+			</div>
 			<button id="stop_button" style="margin: 0.5em;">Stop all</button>
+			<span>Waiting:</span>
+			<span id="waiting-counter"></span>
 		</div>
 		<div id="info_div">
 		</div>
 	`;
 
-	let info_div =  userScriptDoc.getElementById('info_div');
-	log = output.bind(info_div);
+	var info_div =  userScriptDoc.getElementById('info_div');
+	var input = userScriptDoc.getElementById('interval_input');
+	var waitingCounter = userScriptDoc.getElementById('waiting-counter');
 
-	let startButton = userScriptDoc.getElementById('start_button');
-	startButton.addEventListener('click', startNew, false);
 
-	let stopButton = userScriptDoc.getElementById('stop_button');
-	stopButton.addEventListener ('click', stopAll, false);
+	function Poller(name, url, data) {
+		this.name = name;
+		this.url = url || 'xkAction.do';
+		this.data = data;
+		this.selected = false;
+		this.waitingCount = 0;
+	}
+	Poller.prototype.output = function(msg) {
+		outputToElement(info_div, timeTag() + ' Poller ' + this.name + ': ' + msg);
+	};
+	Poller.prototype.updateWaitingCounter = function() {
+		waitingCounter.innerText = this.waitingCount;
+	};
+	Poller.prototype.handleResponse = function(resp) {
+		var msg = getMessage(resp);
+		if (!this.selected) {
+			this.output('Return: ' + msg);
+		}
+		if (msg.indexOf('成功') != -1) {
+			this.stop();
+			this.selected = true;
+		}
+	};
+	Poller.prototype.start = function () {
+		var int = getInvervalInputValue(input);
+		this.selected = false;
+		if (!this.data) {
+			this.output('Error, form data is undefined');
+		} else {
+			this.output('Started');
+			this.intervalIndicator = setInterval(function () {
+				var http = new XMLHttpRequest();
+				http.open('POST', this.url, true);
+				http.onreadystatechange = function() {
+					if(http.readyState == 4 ) {
+						--this.waitingCount;
+						this.updateWaitingCounter();
+						if (http.status == 200)
+							this.handleResponse(http.responseText);
+					}
+				}.bind(this);
+				http.send(this.data);
+				this.output('A new request sent');
+				++this.waitingCount;
+				this.updateWaitingCounter();
+			}.bind(this), int);
+		}
+	};
+	Poller.prototype.stop = function () {
+		if (this.intervalIndicator) {
+			clearInterval(this.intervalIndicator);
+			delete this.intervalIndicator;
+		}
+		this.output('Stopped');
+	};
+	Poller.prototype.getData = function () {
+		function getFrameEleByName(frame, name) {
+			return frame.contentDocument.getElementsByName(name)[0];
+		}
+
+		var bottomFrame = document.getElementsByName('bottomFrame')[0];
+		var mainFrame = getFrameEleByName(bottomFrame, 'mainFrame');
+		var inforUpContent = getFrameEleByName(mainFrame, 'inforUpContent');
+		var form;
+		if (inforUpContent && (form = getFrameEleByName(inforUpContent, 'xkActionForm'))) {
+			this.data =  new FormData(form);
+			this.output('Data refreshed');
+		} else {
+			this.output('Error, Data refresh failed');
+		}
+	};
+
+	var startButton = userScriptDoc.getElementById('start_button');
+	var stopButton = userScriptDoc.getElementById('stop_button');
+	var refreshButton = userScriptDoc.getElementById('refresh_button');
+
+	var poller = new Poller('main');
+
+	startButton.addEventListener('click', function () {
+		poller.getData();
+		poller.start();
+	});
+	refreshButton.addEventListener('click', function () {
+		poller.getData();
+	});
+	stopButton.addEventListener('click', function () {
+		poller.stop();
+	});
+}());
+
+function getMessage(response) {
+	const pattern = /xs\('(.*)'\);/;
+	const allTagPattern = /<[^>]*>/g;
+
+	var raw_msg = pattern.exec(response);
+	if (raw_msg && raw_msg[1]) {
+		var msg = raw_msg[1].replace(allTagPattern, '');
+		return msg;
+	} else {
+		return 'Unknown response';
+	}
 }
 
-main();
+function getInvervalInputValue(input) {
+	var min = Number(input.getAttribute('min'));
+	if (input.value < min)
+		input.value = min;
+	return input.value;
+}
+
+function timeTag() {
+	var date = new Date();
+	return '[' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ']';
+}
+
+function outputToElement(parentElement, innerHTML, newElementTag) {
+	newElementTag = newElementTag || 'p';
+
+	var newElem = document.createElement(newElementTag);
+	parentElement.insertBefore(newElem, parentElement.firstChild);
+	newElem.innerHTML = innerHTML;
+}
